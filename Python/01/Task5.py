857ad0fe7dc7fc9ed5b66b27464c8b599450369d
angolpow <angolpow@gmail.com>
2020-12-21 11:56:31 +0300

    1 lesson done

--- Python/01/Task5.py
+++ Python/01/Task5.py
@@ -19,4 +19,5 @@ else:
 
 if profit:
     size = int(input("Введите численность: "))
-    print(profit / size)
+    print("Рентабельность: ", round(profit / costs, 2))
+    print("Прибыль на одного сотрудника: ", round(profit / size, 2))
