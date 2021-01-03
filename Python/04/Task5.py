096596616eed3dbf13238d0d93b054ae371a8001
angol <angolpow@gmail.com>
2021-01-03 16:52:27 +0300

    4.5 fixed

--- Python/04/Task5.py
+++ Python/04/Task5.py
@@ -6,4 +6,4 @@
 from functools import reduce
 
 
-print(reduce(lambda x, y: x+y, [i for i in range(100, 1001) if i % 2 == 0]))
+print(reduce(lambda x, y: x*y, [i for i in range(100, 1001) if i % 2 == 0]))
